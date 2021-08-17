0d117141a0d0b02384b426233450dfbd5b5e7728
Kaalaayaa <kalaya.chatelain@yahoo.fr>
2021-08-17 13:48:45 +0200

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,31 @@
+import { useLocaleContext } from '../contexts/LocaleContext';
 import { useProductsContext } from '../contexts/ProductsContext';
 
 export default function Header() {
     const [ products ] = useProductsContext();
+    const [locale, setLocale] =  useLocaleContext()
+
+    const localeOptions = [
+    {locale : "en-us", currency: "$"},
+    {locale: "en-gb", currency: "£"},
+    {locale: "de-de", currency: "€"}
+]
+
+    function handleChange(e) {
+        setLocale(e.target.value)
+        
+    }
 
     return (
         <header>
             <h1>Product manager</h1>
             <p>Now with {products.length} products</p>
+            <p>Select currency</p>
+            <select value={locale} onChange={handleChange}>
+                {localeOptions.map((option) => (
+              <option value={option.locale}>{option.locale}</option>
+            ))}
+            </select>
         </header>
     )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,29 @@
+import { useLocaleContext } from '../contexts/LocaleContext';
 import { useProductsContext } from '../contexts/ProductsContext';
 
+
 export default function Products() {
-    const [ products ] = useProductsContext();
+    const [ products,setProducts ] = useProductsContext();
+    const [ locale] = useLocaleContext()
+
+    function deleteProduct(id){
+        const newProducts = products.filter((item) => { return item.id !== id });
+
+        setProducts(newProducts)
+    }
+
+    let currency = "€"
+
+    if(locale === "en-us"){
+        currency = "$"
+    } else if(locale ==="en-gb"){
+        currency = "£"
+    }
 
     return (
         <div className="Products">
-            {products.map(prod => <div>{prod.name}: {prod.price} &euro;</div>)}
+            {products.map(prod => <div key={prod.id}>{prod.name}: {prod.price}{currency}<button onClick={() => deleteProduct(prod.id)}>delete</button></div>)}
+            {/* {locale.map(options => <div>{options.locale}</div>)} */}
         </div>
     )
 }
